b4346cc607f76837027ca420667011dfc911d050
ChristYoga123 <christianuswibisono@gmail.com>
2025-01-08 08:41:12 +0700

    feat: course data

---
+++ database/migrations/2025_01_08_013442_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->unsignedBigInteger('mentor_id');
+            $table->enum('kategori', ['Programming', 'Cyber Security', 'Design', 'Digital Marketing']);
+            $table->string('judul')->unique();
+            $table->string('slug')->unique();
+            $table->enum('level', ['Semua Level', 'Pemula', 'Menengah', 'Mahir']);
+            $table->enum('tipe', ['Gratis', 'Berbayar']);
+            $table->unsignedBigInteger('harga')->default(0);
+            $table->longText('konten');
+            $table->boolean('is_draft')->default(true);
+            $table->text('drive_url')->nullable();
+
+            $table->foreign('mentor_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
