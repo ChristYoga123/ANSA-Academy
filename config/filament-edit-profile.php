80b964a689791f9c05f40df2f4cae0409d063b45
ChristYoga123 <christianuswibisono@gmail.com>
2025-02-05 14:31:28 +0700

    feat: add edit profile medsos

--- config/filament-edit-profile.php
+++ config/filament-edit-profile.php
@@ -1,5 +1,28 @@
 <?php
 
 return [
-
+    'show_custom_fields' => true,
+    'custom_fields' => [
+        'no_hp' => [
+            'type' => 'text',
+            'label' => 'Nomor HP',
+            'placeholder' => 'Masukkan Nomor HP',
+            'required' => true,
+            'rules' => 'required|numeric',
+        ],
+        'linkedin' => [
+            'type' => 'text',
+            'label' => 'Linkedin',
+            'placeholder' => 'Masukkan Linkedin',
+            'required' => true,
+            'rules' => 'required|string|max:255',
+        ],
+        'instagram' => [
+            'type' => 'text',
+            'label' => 'Instagram',
+            'placeholder' => 'Masukkan Instragram',
+            'required' => true,
+            'rules' => 'required|string|max:255',
+        ],
+    ],
 ];
